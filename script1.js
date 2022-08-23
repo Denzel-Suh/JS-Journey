794f663411a0fa306e10683e636b5f85715bb337
Denzel-Suh <95910419+Denzel-Suh@users.noreply.github.com>
2022-08-23 20:47:00 +0100

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,21 @@
+function getDate(){
+    document.getElementById('demo').innerHTML = Date();
+}
+function replace(){
+    document.getElementById('p1').innerHTML = 'Denzel is awesome';
+}
+function img1(){
+    document.getElementById('image').src = 'img0.jpg';
+}
+function img2(){
+    document.getElementById('image').src = 'img1.jpg';
+}
+function hideImg(){
+    document.getElementById('image').style.display = 'none';
+}
+function showImg(){
+    document.getElementById('image').style.display = 'block';
+}
+function hideNav(){
+    document.getElementById('navbar').style.display = 'block';
+}
